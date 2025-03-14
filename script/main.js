dc20786873207b6528ef84806a52e94b90814f98
syz-bash <syzz.bd@gmail.com>
2025-03-14 20:49:09 +0600

    complete project

--- script/main.js
+++ script/main.js
@@ -12,11 +12,15 @@ let displayCategories=(categories)=>{
     // loop
     for(let cet of categories){
         let createDiv= document.createElement('div')
+        
+
+        
         createDiv.innerHTML= `
-        <button class="btn btn-sm hover:text-white hover:bg-[#FF1F3D]">${cet.category}</button>
+        <button id="btn-${cet.category_id}" onclick="loadId(${cet.category_id})" class="btn btn-sm hover:text-white hover:bg-[#FF1F3D]">${cet.category}</button>
         `
+        
         // appenndChild
-        categorieContainer.appendChild(createDiv);;
+        categorieContainer.appendChild(createDiv);
         
     }
     
@@ -24,37 +28,147 @@ let displayCategories=(categories)=>{
 
 loadCategories()
 
+document.getElementById('search-video')
+.addEventListener('keyup', (event)=>{
+    loadVideo(event.target.value);
+    
+    
+})
+
+// remove active
+let removeActive=()=>{
+    let callActive= document.getElementsByClassName('active');
+    for(let callAc of callActive){
+        callAc.classList.remove('active')
+    }
+    
+}
+
+
+// load ID
+let loadId=(id)=>{
+    
+    let loadId= fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res=> res.json())
+    .then(data=> {
+        removeActive()
+        let clickedBtn= document.getElementById(`btn-${id}`)
+        clickedBtn.classList.add('active')
+        videoContain(data.category)
+    })
+    
+}
+
 // video-container
 
 // load-data
-const loadVideo=()=>{
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+const loadVideo=(searchValue = '')=>{
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchValue}`)
     .then(res=> res.json())
-    .then(data=> videoContain(data.videos))
+    .then(data=> {
+        let btnAll= document.getElementById('btn-all');
+        btnAll.classList.add('active')
+
+        videoContain(data.videos) 
+    })
 }
+
+// video-details
+let loadVideoDetails=(video_id)=>{
+    let url= fetch(`https://openapi.programming-hero.com/api/phero-tube/video/${video_id}`)
+    console.log(video_id);
+    
+
+    let detailsVideo= document.getElementById('video-details').showModal();
+    // data-container
+    let dataContainer= document.getElementById('data_container')
+
+
+    url .then(res=> res.json())
+        .then(data=> {
+            // let container= document.createElement('div')
+            dataContainer.innerHTML=`
+                <h2>Status: ${data.video.others.views}</h2>
+                <h2>Status: ${data.video.category_id}</h2>
+
+            `
+            dataContainer.append(container);
+            console.log(data);
+            
+        })
+        
+    
+    
+}
+
+
 // conatin
+// display-Video
 const videoContain=(videos)=>{
+    
+    
+    document.getElementById('videoContainer').innerText="";
     let callVideoContainer= document.getElementById('videoContainer');
 
+
+    if(videos.length === 0){
+        callVideoContainer.innerHTML=`
+        <div class="col-span-full flex flex-col items-center justify-center py-20 gap-3">
+                <img class="w-[120px]" src="assest/Icon.png" alt="">
+                <h1 class="text-2xl font-bold">Oops!! Sorry, There is no content here</h1>
+            </div>
+        `
+        return
+    }
+
+   
+
+    
     videos.forEach(video => {
-        console.log(video);
         
         let createDiv= document.createElement('div');
         createDiv.innerHTML=`
-        <div class="card bg-base-100  shadow-sm">
-        <figure>
-            <img
-            src="${video.thumbnail}"
-            alt="Shoes" />
-        </figure>
-        <div class="card-body">
-            <h2 class="card-title">${video.title}</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+        <div class="mx-0">
+
+                <div class="card bg-base-100 ">
+                    <figure class="relative">
+                        <img
+                        class="w-full h-[200px] object-cover"
+                        src="${video.thumbnail}"
+                        alt="Shoes" />
+                        <span class="absolute bottom-2 right-2 bg-black text-white p-1 text-xs">3hrs 56 min ago</span>
+                    </figure>
+                    <div class="flex gap-5 p-5 px-0">
+                        <!-- icon image -->
+                        <div>
+                            <div class="avatar">
+                                <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                                    <img src="${video.authors[0].profile_picture}" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="">
+                            <h2 class="text-lg font-medium">
+                                Building a Winning UX Strategy Using the Kano Model
+                            </h2>
+
+                            <p class="text-[#17171770] flex items-center gap-2 py-1">
+                                ${video.authors[0].profile_name}
+                                <span>
+                                    ${video.authors[0].verified === true? 
+                                        `<img class="w-5" src="https://img.icons8.com/?size=48&id=HxdvwPmtGaQL&format=gif" alt=""></img>`:''}
+                                </span>
+                            </p>
+
+                            <p class="text-[#17171770]">
+                                ${video.others.views}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">block</button>
             </div>
-        </div>
-        </div>
         `
         // append child
         callVideoContainer.appendChild(createDiv)
@@ -65,4 +179,4 @@ const videoContain=(videos)=>{
 
     
 }
-loadVideo()
+
